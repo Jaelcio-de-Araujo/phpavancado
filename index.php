e928801c2f4e5dc8f6897e9063ed2ba81971c90a
Jaelcio Araujo <jaelcioaraujo.contato@gmail.com>
2022-12-05 17:29:25 -0300

    Corrigindo retorno

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
-require_once "src/Search.php";
+require __DIR__.'/vendor/autoload.php';
+
+use Jdev\Cepdigital\Search;
+
 
 $busca  = new Search;
 
 $resultado  = $busca -> getAddressFromZipcode('06230060');
-
 print_r($resultado);
--- src/Search.php
+++ src/Search.php
@@ -1,15 +1,20 @@
 <?php
 
+namespace Jdev\Cepdigital;
 class  Search
 {
     private  $url = "https://viacep.com.br/ws/";
 
-    public function getAddressFromZipcode(string $zipCode): array
+    public function getAddressFromZipcode(string $zipCode)
     {
-        $zipCode = preg_replace('/[^0-9]/im','',$zipCode);
+        $zipCode =  preg_replace('/[^0-9]/im','',$zipCode);
 
-        $get = file_get_contents($this->url . $zipCode . "/json");
-        return (array) json_decode($get);
+        $get =      file_get_contents($this->url . $zipCode . "/json");
+
+        $get =      json_decode($get,true);
+
+        return isset($get['cep']) ? $get : null;
+
     }
 }
 
